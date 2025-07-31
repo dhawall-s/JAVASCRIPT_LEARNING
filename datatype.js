6bf6087f06f2c1ba2cb000e04baad0bb896be987
dhawall-s <sainidhawal755@gmail.com>
2025-07-31 12:45:34 +0530

    commit

---
+++ datatype.js
@@ -0,0 +1,21 @@
+let dhawal=10
+console.log(dhawal)
+
+const vote=-1
+console.log(typeof(vote))
+
+let num=10
+console.log("value of num is",num)
+
+const sahi=true
+console.log( typeof sahi)
+
+let ac=9.0;
+console.log(typeof ac)
+
+let balance=null
+console.log(balance)
+
+
+let a=4309093840932403094434343434n
+console.log(typeof a)
