452d8d41e26edb1de25a6fe94a2f23d3eeef5b78
dhawall-s <sainidhawal755@gmail.com>
2025-08-05 22:26:52 +0530

    commit

--- number.js
+++ number.js
@@ -5,3 +5,33 @@
 // console.log(num2==num3)
 // console.log(num2)
 // console.log(typeof num2)
+
+// let num=231.689
+// console.log(num.toFixed(1))
+
+// console.log(num.toPrecision(4))
+
+// console.log(num.toExponential(3))
+
+// console.log( typeof num.toString())
+
+// console.log(num.valueOf())
+
+// console.log(typeof num)
+
+// console.log(Math.E);
+// console.log(Math.LN10);
+// console.log(Math.SQRT1_2)
+// console.log(Math.PI)
+// console.log(Math.max(2,3))
+
+// console.log(Math.random()*100)
+
+
+// let num1=23.45
+// console.log(Math.floor(num1))
+// console.log(Math.ceil(num1))
+
+console.log(Math.floor(Math.random()*10)+11)
+
+
